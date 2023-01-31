1177dc5a3152683db663720c2853230c4751b224
Sashaegypt <107362302+Sashaegypt@users.noreply.github.com>
2023-01-31 18:13:44 +0100

    make hw

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types'
+
+export const Notification = ({ message }) => {
+    return <p>{message}</p>;
+};
+
+Notification.PropTypes = {
+    message: PropTypes.string.isRequired,
+};
